8597551d67fa15ff70c0a5e11e959375dd321507
Minung Han <hmu332233@gmail.com>
2022-11-08 17:04:32 +0000

    컨셉용 about 페이지 추가

--- pages/api/widget/index.ts
+++ pages/api/widget/index.ts
@@ -1,20 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Data>,
 ) {
-
   const svg = `
-  <svg fill="none" width="560" height="315" xmlns="http://www.w3.org/2000/svg">
+  <svg fill="none" width="800" height="1200" xmlns="http://www.w3.org/2000/svg">
   <foreignObject width="100%" height="100%">
     <div xmlns="http://www.w3.org/1999/xhtml">
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/3X8vrq2IjSg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+      <iframe width="800" height="1200" src="https://blog-new.minung.dev/about-short" title="about me" frameborder="0" allow="clipboard-write;"></iframe>
     </div>
   </foreignObject>
   </svg>
